53ffe34b85fd58d7d929d8b49388c9f55761f4dd
veronicapava <veronicapava@gmail.com>
2022-04-04 00:17:04 -0500

    Añade SemanticUI para darle estilos a la app

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { _getPhones } from '../utils/_DATA'
 import { receivePhones } from '../actions/phones'
 import Container from './Container'
+import 'semantic-ui-css/semantic.min.css'
 
 
 class App extends Component {
